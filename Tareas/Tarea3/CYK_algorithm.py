739a5c0d36c3a7779459c666c577eeeacfa7a755
georgeif2212 <jinfante2212@gmail.com>
2024-05-09 17:38:16 -0600

    feat: new function to fill the first column of the table

--- Tareas/Tarea3/CYK_algorithm.py
+++ Tareas/Tarea3/CYK_algorithm.py
@@ -15,21 +15,43 @@ def createMatrix(stringLength: int) -> List[List[Set]]:
     return table
 
 
+def fillFirstColumn(
+    grammar: Dict[str, List[List[str]]], string: str, table: List[List[Set]]
+):
+    stringLength = len(string)
+    # * Fill first column
+    for i in range(stringLength):
+        character = string[i]
+
+        # * Extract non terminal symbol side and the production list
+        for nonTerminalSymbol, production_list in grammar.items():
+
+            # * for loop to iterate between production list
+            for terminalSymbol in production_list:
+
+                # * if is terminalSymbol and match with the character add to the first column
+                if len(terminalSymbol) == 1 and terminalSymbol == character:
+                    table[i][0].add(nonTerminalSymbol)
+
+
 def cyk(grammar: Dict[str, List[List[str]]], string: str):
 
     stringLength = len(string)
     # Initialize the table
     table = createMatrix(stringLength)
 
-    # Llenar la primera columna con producciones terminales
-    for j in range(stringLength):
-        character = string[j]  # Carácter actual
-        for lhs, rhs_list in grammar.items():
-            for rhs in rhs_list:
-                # Si el lado derecho es un terminal y coincide con el carácter actual
-                if len(rhs) == 1 and rhs == character:
-                    table[j][0].add(lhs)  # Agrega el símbolo no terminal a la tabla
-    # print(table)
+    fillFirstColumn(grammar, string, table)
+
     for row in table:
         print(row)
+
+    for i in range(stringLength - 1):
+        for j in range(1, stringLength):
+            for k in range(j - 1):
+                # Hacer producto cartesiano para obtener opciones
+                # opciones = Vik X Vi + k, j-k
+                return
+
+    # print(table)
+
     print("--------------------------------------------------------------")
