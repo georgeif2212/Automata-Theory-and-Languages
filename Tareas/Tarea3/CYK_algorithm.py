7a79aa130e1802876ebbe028b156d54afcd342cb
georgeif2212 <jinfante2212@gmail.com>
2024-05-11 13:43:46 -0600

    feat: cyk algorithm almost finished

--- Tareas/Tarea3/CYK_algorithm.py
+++ Tareas/Tarea3/CYK_algorithm.py
@@ -31,3 +31,26 @@ def fillFirstColumn(
                     table[i][0].add(non_terminal_symbol)
 
 
+def cyk(grammar: Dict[str, List[List[str]]], string: str):
+    string_length = len(string)
+    # Initialize the table
+    table = createMatrix(string_length)
+
+    fillFirstColumn(grammar, string, table)
+
+    # * loop to iterate between columns
+    for j in range(1, string_length):
+        # * loop to iterate between rows
+        for i in range(string_length - 1):
+            # * loop to iterate the number of cartesian product
+            for k in range(j):
+                if i + k + 1 < string_length:
+                    first_set = table[i][k]
+                    second_set = table[i + k + 1][j - k - 1]
+                    for non_terminal_symbol, production_list in grammar.items():
+                        # print(f"non_terminal_symbol:{non_terminal_symbol}, productionList:{production_list}")
+                        for production in production_list:
+                            if production[0] in first_set and production[1] in second_set:
+                                table[i][j].add(non_terminal_symbol)  # Añadir el no terminal a la tabla
+    for row in table:
+        print(row)
