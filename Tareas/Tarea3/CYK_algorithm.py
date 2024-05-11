95ef4a36b10001e548fddea16fd23a1c4419277c
georgeif2212 <jinfante2212@gmail.com>
2024-05-11 13:43:09 -0600

    feat: fill first Column function finsihed

--- Tareas/Tarea3/CYK_algorithm.py
+++ Tareas/Tarea3/CYK_algorithm.py
@@ -1,14 +1,14 @@
 from typing import Dict, List, Set
 
 
-def createMatrix(stringLength: int) -> List[List[Set]]:
+def createMatrix(string_length: int) -> List[List[Set]]:
     table = []
 
-    # * stringLength rows are created
-    for i in range(stringLength):
+    # * string_length rows are created
+    for i in range(string_length):
         row = []
         # * stringLength columns are created and a set is added
-        for j in range(stringLength):
+        for j in range(string_length):
             row.append(set())
         table.append(row)
 
@@ -18,40 +18,16 @@ def createMatrix(stringLength: int) -> List[List[Set]]:
 def fillFirstColumn(
     grammar: Dict[str, List[List[str]]], string: str, table: List[List[Set]]
 ):
-    stringLength = len(string)
+    string_length = len(string)
     # * Fill first column
-    for i in range(stringLength):
+    for i in range(string_length):
         character = string[i]
-
         # * Extract non terminal symbol side and the production list
-        for nonTerminalSymbol, production_list in grammar.items():
-
+        for non_terminal_symbol, production_list in grammar.items():
             # * for loop to iterate between production list
-            for terminalSymbol in production_list:
+            for terminal_symbol in production_list:
+                # * if is terminal_symbol and match with the character add to the first column
+                if len(terminal_symbol) == 1 and terminal_symbol == character:
+                    table[i][0].add(non_terminal_symbol)
 
-                # * if is terminalSymbol and match with the character add to the first column
-                if len(terminalSymbol) == 1 and terminalSymbol == character:
-                    table[i][0].add(nonTerminalSymbol)
 
-
-def cyk(grammar: Dict[str, List[List[str]]], string: str):
-
-    stringLength = len(string)
-    # Initialize the table
-    table = createMatrix(stringLength)
-
-    fillFirstColumn(grammar, string, table)
-
-    for row in table:
-        print(row)
-
-    for i in range(stringLength - 1):
-        for j in range(1, stringLength):
-            for k in range(j - 1):
-                # Hacer producto cartesiano para obtener opciones
-                # opciones = Vik X Vi + k, j-k
-                return
-
-    # print(table)
-
-    print("--------------------------------------------------------------")
