c800cd5e72a50784a623d0b17cc2e72aebc5d90e
Jorge Infante <jinfante2212@gmail.com>
2024-08-23 12:32:35 -0600

    feat: header to create nodes from the tree

---
+++ Translators/exam/robot_header.h
@@ -0,0 +1,36 @@
+
+typedef enum {typeCon, typeId, typeOpr} nodeEnum;
+
+/* constants */
+typedef struct
+{
+    nodeEnum type;
+    int value; /* value of constant */
+} conNodeType;
+
+/* identifiers */
+typedef struct
+{
+    nodeEnum type;
+    int i; /* índice en tabla de símbolos */
+} idNodeType;
+
+/* operators */
+typedef struct
+{
+    nodeEnum type;
+    int oper;                 /* operator */
+    int nops;                 /* number of operands */
+    union nodeTypeTag *op[1]; /* operands (expandable) */
+} oprNodeType;
+
+typedef union nodeTypeTag
+{
+    nodeEnum type; /* type of node */
+
+    conNodeType con; /* constants */
+    idNodeType id;   /* identifiers */
+    oprNodeType opr; /* operators */
+} nodeType;
+
+extern int sym[26];
